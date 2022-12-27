b00618bee41f0788279fed9323cc87cca2cf1680
K.K.S.Nayanthara <2020cs120@stu.ucsc.cmb.ac.lk>
2022-12-27 14:05:52 +0530

    add customer dashboard css

--- app/controllers/Orders.php
+++ app/controllers/Orders.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+if (!isset($_SESSION['user_id']) || !isset($_SESSION['role'])) {
+    header('Location: ' . BASEURL . '/home');
+}
 
 class Orders extends Controller{
     function __construct(){
@@ -6,10 +10,25 @@ class Orders extends Controller{
     }
 
     function dealer(){
+        $dealer_id = $_SESSION['user_id'];
         $data['navigation'] = 'orders';
         $dealer_details = $this->model('Dealer')->getDealerImage($dealer_id);
         $row = mysqli_fetch_assoc($dealer_details);
         $data['image'] = $row['image'];
         $this->view('dashboard/dealer', $data);
     }
+
+
+    //customer past all reservtions
+    function customer(){
+        $customer_id = $_SESSION['user_id'];
+        $data['navigation'] = 'myreservation';
+        $customer_details = $this->model('Customer')->getCustomerImage($customer_id);
+        $row = mysqli_fetch_assoc($customer_details);
+        $data['image'] = $row['image'];
+        $this->view('customer/allmyreservation', $data);
+    }
+
+
+
 }
