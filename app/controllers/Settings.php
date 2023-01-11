ef89bd16b2215cf45f8d3262bf63224eb238aed9
Hashini Thilinika <2020cs187@stu.ucsc.cmb.ac.lk>
2023-01-11 20:12:30 +0530

    changes

--- app/controllers/Settings.php
+++ app/controllers/Settings.php
@@ -20,16 +20,22 @@ class Settings extends Controller {
         $this->view('distributor/settings', $data);
     }
 
-    // public function profileimg() {
-    //     $user_id = $_SESSION['user_id'];
-    //     $data['navigation'] = 'settings';
 
-    //     $distributor_details = $this->model('Distributor')->getDistributorImage($user_id);
-    //     $row = mysqli_fetch_assoc($distributor_details);
-    //     $data['image']=$row['image'];
+    public function updateprofile() {
+        $user_id = $_SESSION['user_id'];
+        $data['navigation'] = 'settings';
 
-    //     $this->profileimg($data);   
-    // }
+        $distributor_details = $this->model('Distributor')->getDistributorImage($user_id);
+        $row = mysqli_fetch_assoc($distributor_details);
+        $data['image']=$row['image'];
+
+        // $data['profile'] = $this->model("Distributor")->viewprofile($user_id);
+
+        $this->view('distributor/settings', $data);
+    }
+
+
+
 }
 
 
