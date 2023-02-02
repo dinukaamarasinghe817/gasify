0b789d9acb288a388c78b0e3900481ff836ae1d0
Dinuka Ashan Amarasinghe <2020cs010@stu.ucsc.cmb.ac.lk>
2023-02-02 21:32:26 +0530

    charts css

--- public/js/dealer.js
+++ public/js/dealer.js
@@ -41,3 +41,19 @@ function poinfo(poid) {
     }
     xhr.send();
 }
+
+function callthisfunc_onclick() {
+    let form = document.querySelector("div form");
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost/mvc/controller/method/', true);
+    xhr.onload = ()=>{
+        if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
+            let data = xhr.response;
+            if(data){
+                // do what ever you want to do with the data
+            }
+        }
+    }
+    let formData = new FormData(form);
+    xhr.send(formData);
+}
