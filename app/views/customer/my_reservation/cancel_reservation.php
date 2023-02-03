e0bde02d53aae3639b90c7b82e596d4b6862d886
K.K.S.Nayanthara <2020cs120@stu.ucsc.cmb.ac.lk>
2023-02-03 11:46:52 +0530

    dealers

---
+++ app/views/customer/my_reservation/cancel_reservation.php
@@ -0,0 +1,21 @@
+<?php
+$header = new Header("dealer");
+$sidebar = new Navigation('customer',$data['navigation']);
+?>
+
+<section class="body">
+    <?php
+        // call the default header for yout interface
+        $bodyheader = new BodyHeader($data);
+        // call whatever the component you need to show
+        // $bodycontent = new Body('viewmyreservation', $data);
+        $prompt = new Prompt('verification',$data);
+
+    ?>
+
+     
+</section>
+
+<?php
+$footer = new Footer();
+?>
--- public/components/prompt.php
+++ public/components/prompt.php
@@ -33,6 +33,26 @@ class Prompt{
     //         </div>';
     // }
 
+    // public function confirm_cancel_reservation(){
+    //     echo '<div class="verification">
+    //             <h1>Verification Status</h1>
+    //             <div class="box">
+    //                 <div>
+    //                     <h2>Stock</h2>
+    //                     <p>pending</p>
+    //                 </div>
+    //                 <img src="'.BASEURL.'/public/img/icons/accept.png" alt="">
+    //             </div>
+    //             <div class="box">
+    //                 <div>
+    //                     <h2>Payment</h2>
+    //                     <p>pending</p>
+    //                 </div>
+    //                 <img src="'.BASEURL.'/public/img/icons/pending.png" alt="">
+    //             </div>
+    //             <button onclick="viewinfo(); return false;">OK</button>
+    //         </div>';
+    // }
     public function toast($toast){
         echo '<div id="toast" class="'.$toast['type'].'">
                 <div class="container-1">';
