651925a72a7b9372ca4f896330c79d9fd9a7d04b
Hashini Thilinika <2020cs187@stu.ucsc.cmb.ac.lk>
2023-04-19 12:08:23 +0530

    rs changes, report changes

--- app/views/distributor/placed_accepted.php
+++ app/views/distributor/placed_accepted.php
@@ -103,7 +103,7 @@ $sidebar = new Navigation('distributor',$data['navigation']);
                                                 <td>'.$row2['product_id'].'</td>
                                                 <td>'.$row2['unit_price'].'</td>
                                                 <td>'.$row2['quantity'].'</td>
-                                                <td>'.$subtotal.'</td>    
+                                                <td>'.$subtotal.'.00</td>    
                                             </tr>';
                                             $total += $subtotal;
 
@@ -113,7 +113,7 @@ $sidebar = new Navigation('distributor',$data['navigation']);
                                             <td><b>Total Amount</b></td>
                                             <td></td>
                                             <td></td>
-                                            <td><b>'.$total.'</b></td>
+                                            <td><b>'.$total.'.00</b></td>
                                         </tr>';
 
                                         $output .= '
