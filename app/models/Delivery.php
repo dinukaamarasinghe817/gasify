49a02dab432688cc4d885449653d28ed51922c1e
K.K.S.Nayanthara <2020cs120@stu.ucsc.cmb.ac.lk>
2023-05-03 10:29:40 +0530

    COMPANY error

--- app/models/Delivery.php
+++ app/models/Delivery.php
@@ -55,7 +55,7 @@ class Delivery extends Model
             $city=$row['city'];
         }
         //print_r(gettype($city));
-        $result = $this->Query("SELECT reservation.order_id,reservation.place_date,reservation.place_time,customer.customer_id,reservation_include.product_id,reservation_include.quantity,product.weight,customer.city,customer.street,customer.contact_no,users.first_name,users.last_name,dealer.city AS dcity,dealer.street AS dstreet FROM reservation INNER JOIN reservation_include ON reservation.order_id=reservation_include.order_id INNER JOIN product ON reservation_include.product_id=product.product_id INNER JOIN customer ON reservation.customer_id=customer.customer_id AND reservation.order_state='Accepted' AND reservation.collecting_method='Delivery' INNER JOIN users ON users.user_id=customer.customer_id INNER JOIN dealer ON reservation.dealer_id=dealer.dealer_id AND dealer.city=$city");
+        $result = $this->Query("SELECT reservation.order_id,reservation.place_date,reservation.place_time,customer.customer_id,reservation_include.product_id,reservation_include.quantity,product.weight,customer.city,customer.street,customer.contact_no,users.first_name,users.last_name,dealer.city AS dcity,dealer.street AS dstreet FROM reservation INNER JOIN reservation_include ON reservation.order_id=reservation_include.order_id INNER JOIN product ON reservation_include.product_id=product.product_id INNER JOIN customer ON reservation.customer_id=customer.customer_id AND reservation.order_state='Accepted' AND reservation.collecting_method='Delivery' INNER JOIN users ON users.user_id=customer.customer_id INNER JOIN dealer ON reservation.dealer_id=dealer.dealer_id AND dealer.city='$city");
         return $result;
     }
     public function getCurrentDeliveries(){
