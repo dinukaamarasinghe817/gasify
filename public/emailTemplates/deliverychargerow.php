bd3922af2417263789c6f262a1b5644daae08f1b
Dinuka Ashan Amarasinghe <2020cs010@stu.ucsc.cmb.ac.lk>
2023-05-08 10:20:57 +0530

    notify delivery people via email when delivery charges updated

---
+++ public/emailTemplates/deliverychargerow.php
@@ -0,0 +1,4 @@
+<tr>
+    <th align="center" style="vertical-align: middle; font-family: 'Montserrat',sans-serif; font-size: 12px; font-weight: 500">{MIN} - {MAX} Km</th>
+    <th align="center" style="vertical-align: middle; font-family: 'Montserrat',sans-serif; font-size: 12px; font-weight: 500">{DELIVERY_CHARGE}</th>
+</tr>
