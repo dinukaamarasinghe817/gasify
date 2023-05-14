fbaef1bc239781f9e0fd450cfd5ce809910e9209
Hashini Thilinika <2020cs187@stu.ucsc.cmb.ac.lk>
2023-05-14 11:51:52 +0530

    chnge

--- app/models/Distributor.php
+++ app/models/Distributor.php
@@ -378,7 +378,8 @@ class Distributor extends Model
                 }
             }
 
-            if($flag){  //reduce stock from distirubtor keep and add it to the dealer keep
+            if($flag){  
+                //reduce stock from distirubtor keep and add it to the dealer keep
                 $query2 = $this->Query("SELECT i.product_id as product_id, i.quantity as quantity, o.dealer_id as dealer_id
                 FROM purchase_order o inner join purchase_include i
                 ON o.po_id = i.po_id 
